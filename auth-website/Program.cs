4ea134b2a7d08b2bb1ddaab9cc8eae36474350e8
Jared Holgate <jaredholgate@microsoft.com>
2023-01-16 17:45:36 +0000

    Add B2C login

--- auth-website/Program.cs
+++ auth-website/Program.cs
@@ -1,7 +1,36 @@
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+        .AddMicrosoftIdentityWebApp(options => {
+            builder.Configuration.GetSection("AzureADB2C").Bind(options);
+            options.ClientSecret = Environment.GetEnvironmentVariable("B2CClientSecret");
+            options.Instance = $"https://{Environment.GetEnvironmentVariable("B2CDomainPrefix")}.b2clogin.com";
+            options.Domain = $"{Environment.GetEnvironmentVariable("B2CDomainPrefix")}.onmicrosoft.com";
+            options.TenantId = Environment.GetEnvironmentVariable("B2CTenantId");
+            options.ClientId = Environment.GetEnvironmentVariable("B2CClientId");
+        });
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to 
+    // the default policy
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+builder.Services.AddRazorPages(options => {
+    options.Conventions.AllowAnonymousToPage("/Index");
+})
+.AddMvcOptions(options => { })
+.AddMicrosoftIdentityUI();
+
+IdentityModelEventSource.ShowPII = true;
 
 var app = builder.Build();
 
@@ -13,11 +42,14 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
